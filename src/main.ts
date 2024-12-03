72a1b2f16d209e47c19d8a739a000d30c7fa2710
Tejas Mahajan <tejasmahajan416@gmail.com>
2024-12-03 22:33:01 +0530

    added env to project

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,19 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  app.enableCors();
 
+  const nodeOriginPrefix = process.env.NODE_ORIGIN_PREFIX;
+  app.setGlobalPrefix(nodeOriginPrefix);
+
+  app.enableCors();
+  
   // Swagger
+  const swaggerUrl = `${nodeOriginPrefix}/swagger`;
+  const swaggerJsonUrl = `${swaggerUrl}/json`;
+
   const config = new DocumentBuilder()
     .setTitle('NestJS Swagger')
-    .setExternalDoc('API JSON Documentation', 'api/swagger/json')
+    .setExternalDoc('API JSON Documentation', swaggerJsonUrl)
     .setVersion('1.0')
     .addServer(process.env.NODE_ORIGIN_LOCAL, 'Local')
     .addServer(process.env.NODE_ORIGIN_LOCAL_DEVELOPMENT, 'Local Development')
@@ -32,16 +38,18 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('api/swagger', app, document, {
+  SwaggerModule.setup(swaggerUrl, app, document, {
     swaggerOptions: {
       persistAuthorization: true,
     },
-    jsonDocumentUrl: 'api/swagger/json',
+    jsonDocumentUrl: swaggerJsonUrl,
   });
 
-  const port = process.env.NODE_PORT ?? 3000;
+  const port = +process.env.NODE_PORT || 3000;
   await app.listen(port, '0.0.0.0');
-  const logger = new Logger();
-  logger.debug(`This application is running on: ${await app.getUrl()}/api/swagger`);
+
+  const logger = new Logger('NestApplication');
+  const appUrl = `${await app.getUrl()}/${swaggerUrl}`;
+  logger.log(`Nest application is running on: ${appUrl}`);
 }
 bootstrap();
